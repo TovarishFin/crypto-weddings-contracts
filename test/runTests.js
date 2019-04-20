4f73a8850e8152785efeb7a1b17d71a30159977e
TovarishFin <tovarishfin@gmail.com>
2019-04-20 21:37:37 +0200

    global test setup

---
+++ test/runTests.js
@@ -0,0 +1,31 @@
+const chai = require('chai')
+const { solidity } = require('ethereum-waffle')
+chai.use(solidity)
+const { expect } = chai
+
+global.expect = expect
+
+const Mocha = require('mocha')
+const fs = require('fs')
+const path = require('path')
+
+// Instantiate a Mocha instance.
+const mocha = new Mocha()
+
+const testDir = 'test/tests'
+
+// Add each .js file to the mocha instance
+// eslint-disable-next-line security/detect-non-literal-fs-filename
+fs.readdirSync(testDir)
+  .filter(function(file) {
+    // Only keep the .js files
+    return file.substr(-3) === '.js'
+  })
+  .forEach(function(file) {
+    mocha.addFile(path.join(testDir, file))
+  })
+
+// Run the tests.
+mocha.run(function(failures) {
+  process.exitCode = failures ? 1 : 0 // exit with non-zero status if there were failures
+})
---
+++ test/tests/Wedding.js
@@ -0,0 +1,8 @@
+describe('when using core Wedding functionality', () => {
+  it('should say what up?', () => {
+    expect(true).to.equal(true)
+
+    // eslint-disable-next-line
+    console.log('what up?')
+  })
+})
