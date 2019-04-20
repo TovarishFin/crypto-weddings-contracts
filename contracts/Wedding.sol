3dafbfdec77c4ba8a7f0d3f9d52da92b0d30dd53
TovarishFin <tovarishfin@gmail.com>
2019-04-20 14:37:40 +0200

    creating alternative, wedding contract

--- contracts/Wedding.sol
+++ contracts/Wedding.sol
@@ -14,6 +14,7 @@ contract Wedding {
   bool public married;
   uint256 public dateMarried;
   WeddingType public weddingType;
+  Stage public stage;
 
   enum WeddingType {
     Traditional,
@@ -24,10 +25,40 @@ contract Wedding {
   }
 
   enum Stage {
-    Proposed,
-    Answering,
+    Uninitialized,
+    Initialized,
+    InProgress,
     Married,
     Divorced
+  }
+
+  modifier atStage(
+    Stage _stage
+  ) {
+    require(stage = _stage);
+
+    _;
+  }
+
+  modifier atEitherStage(
+    Stage _stage1,
+    Stage _stage2
+  ) {
+    require(
+      stage == _stage1 ||
+      stage ==  _stage2
+    );
+
+    _;
+  }
+
+  modifier onlyFiance() {
+    require(
+      msg.sender == partner1 ||
+      msg.sener == partner2
+    );
+
+    _;
   }
 
   function initialize(
@@ -38,6 +69,7 @@ contract Wedding {
     WeddingType _weddingType
   )
     external
+    atStage(Stage.Uninitialized)
   {
     require(_p1Address != address(0));
     require(_p2Address != address(0)); 
@@ -49,6 +81,78 @@ contract Wedding {
     partner2 = _p2Address;
     p2Name = _p22ame;
     weddingType = _weddingType;
-  }   
+
+    stage = Stages.Initialized;
+  }
+
+  function acceptProposal()
+    external
+    onlyFiance
+    atEitherStage(Stage.Initialized, Stage.InProgress)
+  {
+    msg.sender == partner1 
+      ? p1Answer = true 
+      : p2Answer = true;
+
+    if (p1Answer && p2Answer) {
+      married = true;
+      dateMarried = block.timestamp;
+      stage = Stage.Married;
+
+      return;
+    }
+    
+    stage = Stage.InProgress;
+  }
+
+  function rejectProposal()
+    external
+    onlyFiance
+    atEitherStage(Stage.Initialized, Stage.InProgress)
+  {
+    msg.sender == partner1
+      ? selfdestruct(partner1)
+      : selfdestruct(partern2);
+  }
+
+  function updateWeddingPhoto(
+    string calldata _weddingPhoto
+  )
+    external
+    onlyFiance
+    atStage(stage.Married)
+  {
+    weddingPhoto = _weddingPhoto;
+  }
+
+  function divorce()
+    external
+    onlyFiance
+    atStage(Stage.Married)
+  {
+    require(address(this).balance == 0);
+
+    msg.sender == parter1
+      ? p1Answer = false
+      : p2Answer = false;
+
+    if (!p1Answer && !p2Answer) {
+      selfdestruct(partner1)
+    }
+  }
+
+  function claimWeddingGifts()
+    external
+    onlyFiance
+  {
+    msg.sender.transfer(address(this).balance);
+  }
+
+  function()
+    external
+    payable
+  {
+    
+  }
 }     
       
--- migrations/1_deploy_contracts.js
+++
@@ -1,7 +0,0 @@
-const WeddingManager = artifacts.require('WeddingManager')
-
-module.exports = async (deployer, network, accounts) => {
-  deployer.deploy(WeddingManager)
-
-  global.accounts = accounts
-}
